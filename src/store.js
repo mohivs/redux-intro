9761923b6fc44cbac7b6eeca7a374e186f6cec88
mohsen <mohisls2012@gmail.com>
2024-06-30 22:01:36 +0330

    dsfsdf

--- src/store.js
+++ src/store.js
@@ -1,7 +1,10 @@
-import { createStore } from "redux";
-const initialState = { balance: 0, loan: 0, loanPurpose: "" };
+import { type } from "@testing-library/user-event/dist/type";
+import { combineReducers, createStore } from "redux";
+const initialStateAccount = { balance: 0, loan: 0, loanPurpose: "" };
 
-function reducer(state = initialState, action) {
+const initialStateCustomer = { fullName: "", nationalID: "", createdAt: "" };
+
+function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
     case "account/deposite":
       return { ...state, balance: state.balance + action.payload };
@@ -9,7 +12,12 @@ function reducer(state = initialState, action) {
       return { ...state, balance: state.balance - action.payload };
     case "account/requestLoan":
       if (state.loan > 0) return state;
-      return { ...state, loan: action.payload };
+      return {
+        ...state,
+        loan: action.payload.loan,
+        loanPurpose: action.payload.loanPurpose,
+        balance: state.balance + action.payload.loan,
+      };
     case "account/payLoan":
       return {
         ...state,
@@ -22,7 +30,53 @@ function reducer(state = initialState, action) {
   }
 }
 
-const store = createStore(reducer);
+function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        createdAt: action.payload.createdAt,
+      };
+    case "customer/updateName":
+      return { ...state, fullName: action.payload };
+
+    default:
+      return state;
+  }
+}
+
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
+});
+
+const store = createStore(rootReducer);
+
 store.dispatch({ type: "account/deposite", payload: 500 });
+store.dispatch({ type: "account/withdraw", payload: 100 });
+
+// console.log(store.getState());
+
+store.dispatch({
+  type: "account/requestLoan",
+  payload: { loan: 600, loanPurpose: "buy ps5" },
+});
+
+// console.log(store.getState());
+
+function createCustomer(fullName, nationalID) {
+  return {
+    type: "customer/createCustomer",
+    payload: { fullName, nationalID, createdAt: new Date().toISOString() },
+  };
+}
+
+function updateName(fullName) {
+  return { type: "customer/updateName", payload: fullName };
+}
+
+store.dispatch(createCustomer("mohsen usefi", "24"));
 
 console.log(store.getState());
